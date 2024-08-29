273a36305b516378030fcafa93d5e826bacd1938
David Harrison Haskell <david.h.haskell@gmail.com>
2024-08-29 18:19:19 +0300

    changes

--- client_side_database/src/Components/LeftSideChild.jsx
+++ client_side_database/src/Components/LeftSideChild.jsx
@@ -2,13 +2,20 @@ import './LeftSideChild.css';
 
 const LeftSideChild = () => {
 
-  return (
-    <div className="LeftSideChild">
-      <h1>Left Side Child</h1>
-      ID: <br />
-      Name: <br />
-      <button >Other Data</button>
-    </div>
-  );
+    return (
+        <div className="LeftSideChild">
+            ID: <br />
+            Name: <br />
+            Email: <br />
+            <div className="buttonContainer">
+                <button className="backgroundButton backgroundButtonOtherData">Other Data</button>
+                <div className="rightButtons">
+                    <button className="backgroundButton">Update</button>
+                    <button className="backgroundButton">Delete</button>
+                </div>
+            </div>
+        </div>
+    );
 }
+
 export default LeftSideChild;
--- client_side_database/src/Components/LeftSideParent.jsx
+++ client_side_database/src/Components/LeftSideParent.jsx
@@ -4,11 +4,11 @@ import LeftSideChild from './LeftSideChild';
 const LeftSideParent = () => {
 
     return (
-        <div className="left-side" style={{ textAlign: "center" }}>
+        <div className="left-side">
             Search: <input type="text" id="search" name="search" style={{ marginRight: "2%" }} />
-            <button>Search</button>
+            <button>Search</button><br /><br />
             <LeftSideChild />
         </div>
-    );
+    ); 
 }
 export default LeftSideParent;
