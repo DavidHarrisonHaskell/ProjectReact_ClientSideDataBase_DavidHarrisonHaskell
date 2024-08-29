20c701008dd9c7af935811f5e84bbfe2f91db7bd
David Harrison Haskell <david.h.haskell@gmail.com>
2024-08-29 16:44:32 +0300

    changes

---
+++ client_side_database/src/Components/LeftSideChild.jsx
@@ -0,0 +1,14 @@
+import './LeftSideChild.css';
+
+const LeftSideChild = () => {
+
+  return (
+    <div className="LeftSideChild">
+      <h1>Left Side Child</h1>
+      ID: <br />
+      Name: <br />
+      <button >Other Data</button>
+    </div>
+  );
+}
+export default LeftSideChild;
--- client_side_database/src/Components/LeftSideParent.jsx
+++ client_side_database/src/Components/LeftSideParent.jsx
@@ -1,11 +1,13 @@
 import './LeftSideParent.css'
-
+import LeftSideChild from './LeftSideChild';
 
 const LeftSideParent = () => {
 
     return (
-        <div className="left-side">
-            <h1>Hello, World!</h1>
+        <div className="left-side" style={{ textAlign: "center" }}>
+            Search: <input type="text" id="search" name="search" style={{ marginRight: "2%" }} />
+            <button>Search</button>
+            <LeftSideChild />
         </div>
     );
 }
